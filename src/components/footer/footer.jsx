00fd32c3450ba44a9ff94bc720db1ad890c25027
Alberto Barrago <albertobarrago@gmail.com>
2024-10-31 12:54:16 +0100

    Rename folders

--- src/components/footer/footer.jsx
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-    return (
-        <footer className="bg-blue-500 text-center text-white p-4">
-            <p>alBz© 2024. MIT license.</p>
-        </footer>
-    )
-}
