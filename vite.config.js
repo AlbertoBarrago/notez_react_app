9253399178e8bab918db0a0869e2abd83e894d0e
Alberto Barrago <albertobarrago@gmail.com>
2024-10-31 17:09:09 +0100

    Fixes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import path from "node:path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/albertobarrago.github.io/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
