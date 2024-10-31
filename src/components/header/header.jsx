9875900d3df356fcf4a7b395b53279c7428d34bb
Alberto Barrago <albertobarrago@gmail.com>
2024-10-31 12:43:01 +0100

    Added Header template

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,29 +1,23 @@
 import {
     NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuIndicator,
     NavigationMenuItem,
-    NavigationMenuLink,
     NavigationMenuList,
-    NavigationMenuTrigger,
-    NavigationMenuViewport,
-} from "@/components/ui/navigation-menu"
-
-
+} from "@/components/ui/navigation-menu";
 
 export default function Header() {
     return (
-        <NavigationMenu>
+        <NavigationMenu className="min-w-full pt-2">
             <NavigationMenuList>
                 <NavigationMenuItem>
-                    <NavigationMenuTrigger>Item one</NavigationMenuTrigger>
-                    <NavigationMenuContent>Item one content</NavigationMenuContent>
+                    <a href="/articles" className="p-4 ">Articles</a>
                 </NavigationMenuItem>
                 <NavigationMenuItem>
-                    <NavigationMenu>Item two</NavigationMenu>
+                    <a href="/about" className="p-4 ">About</a>
+                </NavigationMenuItem>
+                <NavigationMenuItem>
+                    <a href="/contact" className="p-4 ">Contact</a>
                 </NavigationMenuItem>
             </NavigationMenuList>
         </NavigationMenu>
-
-    )
+    );
 }
