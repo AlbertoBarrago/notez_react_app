47ce2f3ad222085316900d75bc391bd6ebff4225
Alberto Barrago <albertobarrago@gmail.com>
2024-11-03 10:20:53 +0100

    Review template for header and footer

--- src/components/layout/footer/index.jsx
+++ src/components/layout/footer/index.jsx
@@ -1,6 +1,6 @@
 export default function Index() {
     return (
-        <footer className="bg-black text-center text-white p-4 text-sm">
+        <footer className="text-center p-4 text-sm">
             <p>🥷🏻 alBz 2024 | <span className="text-[.8em]">MIT license</span> </p>
         </footer>
     )
--- src/components/layout/header/index.jsx
+++ src/components/layout/header/index.jsx
@@ -8,38 +8,50 @@ import {NavLink} from "react-router-dom";
 
 export default function Index() {
     return (
-        <header className="bg-black p-3 min-h-[10px]">
-            <NavigationMenu className="min-w-full">
-                <NavigationMenuList className="flex space-x-4">
+        <header className="p-3 min-h-[10px]">
+            <NavigationMenu className="min-w-full flex-row justify-start">
+                <NavigationMenuList className="space-x-4">
                     <NavigationMenuItem>
                         <NavLink
                             to="/"
-                            className={({ isActive, isPending }) =>
-                                isPending ? "pending p-4" : isActive ? "active text-red-500 p-4" : "text-white p-4"
-                            }
+                            className='tiny5-regular text-2xl'
                         >
-                            Articles
-                        </NavLink>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavLink
-                            to="/about"
-                            className={({ isActive, isPending }) =>
-                                isPending ? "pending p-4" : isActive ? "active text-red-500 p-4" : "text-white p-4"
-                            }
-                        >About
-                        </NavLink>
-                    </NavigationMenuItem>
-                    <NavigationMenuItem>
-                        <NavLink
-                            to="/contact"
-                            className={({ isActive, isPending }) =>
-                                isPending ? "pending p-4" : isActive ? "active text-red-500 p-4" : "text-white p-4"
-                            }
-                        >Contact
+                            Albz Notes
                         </NavLink>
                     </NavigationMenuItem>
                 </NavigationMenuList>
+                <div className="flex-1 justify-end justify-items-end">
+                    <NavigationMenuList className="space-x-4">
+                        <NavigationMenuItem>
+                            <NavLink
+                                to="/"
+                                className={({isActive, isPending}) =>
+                                    isPending ? "pending p-4" : isActive ? "active text-red-500 p-4" : "p-4"
+                                }
+                            >
+                                Articles
+                            </NavLink>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavLink
+                                to="/about"
+                                className={({isActive, isPending}) =>
+                                    isPending ? "pending p-4" : isActive ? "active text-red-500 p-4" : "p-4"
+                                }
+                            >About
+                            </NavLink>
+                        </NavigationMenuItem>
+                        <NavigationMenuItem>
+                            <NavLink
+                                to="/contact"
+                                className={({isActive, isPending}) =>
+                                    isPending ? "pending p-4" : isActive ? "active text-red-500 p-4" : "p-4"
+                                }
+                            >Contact
+                            </NavLink>
+                        </NavigationMenuItem>
+                    </NavigationMenuList>
+                </div>
             </NavigationMenu>
         </header>
     );
