cb879d02ba200eb1b1a5231f63d2f515728ede0f
Alberto Barrago <albertobarrago@gmail.com>
2024-11-10 18:30:16 +0100

    Fixes

--- src/services/auth/index.js
+++ src/services/auth/index.js
@@ -6,43 +6,48 @@ class Auth {
     }
 
     async login(email, password) {
-        const response = axios({
-            method: "post",
-            url: "/api/auth/login",
-            data: {
-                email,
-                password
+        try {
+            const response = axios({
+                method: "post",
+                url: "/api/auth/login",
+                data: {
+                    email,
+                    password
+                }
+            });
+
+            if (response.data.token) {
+                this.user = response.data;
             }
-        });
 
-        if (response.data.token) {
-            this.user = response.data;
+            return response.data;
+
+        } catch (e) {
+            throw new Error(e.message);
         }
 
-        if(response.data.error) {
-            throw new Error(response.data.error);
-        }
-
-        return response.data;
     }
 
     async register(email, username, password) {
-        const response = axios({
-            method: "post",
-            url: "/api/auth/register",
-            data: {
-                email,
-                username,
-                password
+        try {
+            const response = await axios({
+                method: "post",
+                url: "/api/auth/register",
+                data: {
+                    email,
+                    username,
+                    password
+                }
+            });
+            if (response.data.token) {
+                this.user = response.data;
             }
-        })
-        if (response.data.token) {
-            this.user = response.data;
+
+            return response.data;
+
+        } catch (e) {
+            throw new Error(e.message);
         }
 
-        if(response.data.error) {
-            throw new Error(response.data.error);
-        }
-        return response.data;
     }
 }
