75b3bc7e75549b633a63bff40162bad7386f3892
Alberto Barrago <albertobarrago@gmail.com>
2024-11-13 10:45:55 +0100

    Added Delete Event

--- src/routes/articles.jsx
+++ src/routes/articles.jsx
@@ -13,30 +13,44 @@ export default function ArticlesRoute() {
     const [error, setError] = useState(null);
     const notesCache = useRef(null);
 
-    useEffect(() => {
-        const fetchNotes = async () => {
-            if (notesCache.current) {
-                setNotes(notesCache.current);
-                setLoading(false);
-                return;
-            }
+    const fetchNotes = async (isUpdateOrDelete = false) => {
+        if (notesCache.current && !isUpdateOrDelete) {
+            setNotes(notesCache.current);
+            setLoading(false);
+            return;
+        }
 
-            try {
-                const notesFetched = await noteService.getNotes();
-                if (notesFetched) {
-                    console.log(notesFetched);
-                    notesCache.current = notesFetched; // Memorizza la risposta in cache
-                    setNotes(notesFetched);
-                }
-            } catch (error) {
-                console.error('Error fetching notes:', error);
-                setError("Error fetching notes. Please try again.");
-            } finally {
-                setLoading(false);
+        try {
+            const notesFetched = await noteService.getNotes();
+            if (notesFetched) {
+                notesCache.current = notesFetched;
+                setNotes(notesFetched);
             }
-        };
+        } catch (error) {
+            console.error('Error fetching notes:', error);
+            setError("Error fetching notes. Please try again.");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
         fetchNotes();
     }, []);
+
+    const editNote = async (note) => {
+        console.log(note);
+    }
+
+    const deleteNote = async (note) => {
+        try {
+            await noteService.removeNote(note);
+            fetchNotes(true);
+        } catch (error) {
+            console.error('Error deleting note:', error);
+            setError("Error deleting note. Please try again.");
+        }
+    }
 
     if (loading) {
         return <div>Loading...</div>;
@@ -48,7 +62,7 @@ export default function ArticlesRoute() {
         <Layout>
             <div className="flex flex-1 flex-col justify-center items-center space-y-5 p-10">
                 {notes.length > 0 ? (
-                    notes.map(note => <Notes key={note.id} note={note} />)
+                    notes.map(note => <Notes key={note.id} note={note} onEdit={editNote} onDelete={deleteNote} />)
                 ) : (
                     <div>No notes found.</div>
                 )}
