9c2396796ed8135a23d685a306874103fb9c0c7e
Alberto Barrago <albertobarrago@gmail.com>
2024-11-18 16:19:17 +0100

    Improved loader and removed contact

--- src/routes/notes.jsx
+++ src/routes/notes.jsx
@@ -191,7 +191,7 @@ export default function ArticlesRoute() {
         return (
             <Layout>
                 <div className="flex items-center justify-center h-screen">
-                    <div className="animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900"></div>
+                    <div className="animate-spin rounded-full border-t-2 border-b-2 border-primary-900"></div>
                     <p className="text-gray-600 ml-2">Loading...</p>
                 </div>
             </Layout>
