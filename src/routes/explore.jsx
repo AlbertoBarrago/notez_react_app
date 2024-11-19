920e3a794868ea453234d6fbecb635e25f1268c3
Alberto Barrago <albertobarrago@gmail.com>
2024-11-19 16:06:17 +0100

    explore.jsx base improved

--- src/routes/explore.jsx
+++ src/routes/explore.jsx
@@ -1,11 +1,11 @@
 import Layout from "../components/layout/index.jsx";
-// import {useStateContext} from "@/context/index.jsx";
 
 export default function AboutRoute() {
-    // const appContext = useStateContext();
     return (
         <Layout>
-            <h1 className="text-3xl text-center">About</h1>
+            <div className="grid grid-cols-1 gap-4">
+                <h1 className="text-3xl text-center mt-10">Explore articles from others users</h1>
+            </div>
         </Layout>
     )
 }
