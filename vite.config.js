3952f1182bd86eee6debd606ea6351f49e1b1916
Alberto Barrago <albertobarrago@gmail.com>
2024-12-02 23:38:58 +0100

    Added 404.html
    - Fixies

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,23 @@
-import { defineConfig, loadEnv } from 'vite'
+import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {viteStaticCopy} from 'vite-plugin-static-copy'
 import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [
+        react(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: '404.html',
+                    dest: '.'
+                }
+            ]
+        })],
+    resolve: {
+        alias: {
+            "@": path.resolve(__dirname, "./src"),
+        },
     },
-  },
 })
